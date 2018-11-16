9b7148c83b99688c2d57d9c0580dec40f018c316
Astha <yastha.as@gmail.com>
2018-11-16 09:42:26 +0545

    Unnecesary done removed

--- config/database.config.js
+++ config/database.config.js
@@ -1,17 +1,3 @@
 module.exports = {
     url: 'mongodb://localhost:27017/easy-notes'
 }
-
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
